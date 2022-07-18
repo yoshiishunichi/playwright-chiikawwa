a927c8b7395fe294c16baa3198143fdf504dfa6e
吉井駿一 <syun_syun01@icloud.com>
2022-07-18 18:50:07 +0900

    :sparkles: 語尾をちいねえに変更するコードを作成

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
 import { chromium } from "playwright";
 
-const url = "https://salty-ocean-60407.herokuapp.com/posts/index";
+const baseUrl = "https://salty-ocean-60407.herokuapp.com";
+const postsUrl = `${baseUrl}/posts/index`;
+
+const postIndexItemClassName = ".posts-index-item";
 
 const main = async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto(url);
-  await page.screenshot({ path: `example.png` });
+  await page.goto(postsUrl);
+  const linkDivs = await page.$$(postIndexItemClassName);
+  const hrefs = await Promise.all(
+    linkDivs.map(async (linkDiv) => {
+      const link = await linkDiv.$("a");
+      const href = await link?.getAttribute("href");
+      return href;
+    })
+  );
+  const filteredHrefs = [...new Set(hrefs)];
+
+  for (let i = 0; i < filteredHrefs.length; i++) {
+    const href = filteredHrefs[i];
+    await page.goto(`${baseUrl}${href}/edit`);
+    const form = (await page.$$(`form`))[1];
+    const textarea = await form.$("textarea");
+    const button = await form.$("input[type='submit']");
+    const content = (await textarea?.innerHTML()) || "";
+    const reg = /.*ちいねえ/;
+    if (!reg.test(content)) {
+      const newContent = content ? `${content}ちいねえ` : "ちいねえ";
+      await textarea?.fill(newContent);
+      await button?.click({ force: true });
+    }
+  }
   await browser.close();
 };
 
